6632e94beb7dc0b1aa8c00d8fbca6950a34879cc
lauraquintero24 <laurasquintero@unicauca.edu.co>
2025-11-24 11:05:21 -0500

    Add files via upload

---
+++ alu.vhd
@@ -0,0 +1,134 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity alu is
+	port
+	(
+		-- Input ports
+		A	: in  std_logic_vector(7 downto 0);
+		B	: in  std_logic_vector(7 downto 0);
+		ALU_Sel : std_logic_vector(1 downto 0);
+		Result : out std_logic_vector (7 downto 0);
+		NZVC : out std_logic_vector (3 downto 0)
+	);
+end alu;
+
+
+architecture arch_alu of alu is
+
+begin
+
+ALU_PROCESS : process (A, B, ALU_Sel)
+	variable Sum_uns : unsigned (8 downto 0);
+	variable Diff_uns : unsigned (8 downto 0);
+	begin 
+		if (ALU_Sel = "00") then -- ADDITION
+			--- Sum Calculation ----------------------------
+			Sum_uns := unsigned ('0' & A) + unsigned ('0' & B);
+			Result <= std_logic_vector(Sum_uns(7 downto 0));
+		
+			---Negative Flag (N)----------------------------
+			NZVC(3) <= Sum_uns(7);
+		
+			---Zero Flag (Z)----------------------------------
+			if (Sum_uns(7 downto 0) = x"00") then
+				NZVC(2) <= '1';
+			else 
+				NZVC(2) <= '0';
+			end if;
+		
+			--- Overflow Flag (V)-----------------------------
+			if ((A(7)='0' and B(7)='0' and Sum_uns(7)='1') or
+				(A(7)='1' and B(7)='1' and Sum_uns(7)='0')) then
+				NZVC(1) <= '1';
+			else 
+				NZVC(1) <= '0';
+			end if;
+		
+			--- Carry Flag (C)---------------------------------
+			NZVC(0) <= Sum_uns(8);
+	
+	--- Other ALU functionality goes here
+	   elsif (ALU_Sel = "01") then  -- SUBTRACTION
+		      --- Diff Calculation ----------------------------
+            Diff_uns := unsigned('0' & A) - unsigned('0' & B);
+            Result <= std_logic_vector(Diff_uns(7 downto 0));
+				
+             ---Negative Flag (N)----------------------------
+				NZVC(3) <= Diff_uns(7);
+				
+				---Zero Flag (Z)----------------------------------
+            if (Diff_uns(7 downto 0) = x"00") then
+					NZVC(2) <= '1';
+            else 
+               NZVC(2) <= '0';
+            end if;
+
+            --- Overflow Flag (V)-----------------------------
+				if ((A(7)='0' and B(7)='1' and Diff_uns(7)='1') or
+               (A(7)='1' and B(7)='0' and Diff_uns(7)='0')) then
+               NZVC(1) <= '1';
+            else 
+               NZVC(1) <= '0';
+            end if;
+            
+				--- Carry Flag (C)---------------------------------
+            NZVC(0) <= Diff_uns(8);
+
+        --elsif (ALU_Sel = "010") then  -- AND
+            --Result <= A and B;
+            --NZVC(3) <= Result(7);
+            --if (Result = x"00") then NZVC(2) <= '1'; else NZVC(2) <= '0'; end if;
+            --NZVC(1) <= '0';
+            --NZVC(0) <= '0';
+
+        --elsif (ALU_Sel = "011") then  -- OR
+            --Result <= A or B;
+            --NZVC(3) <= Result(7);
+            --if (Result = x"00") then NZVC(2) <= '1'; else NZVC(2) <= '0'; end if;
+            --NZVC(1) <= '0';
+            --NZVC(0) <= '0';
+
+        --elsif (ALU_Sel = "100") then  -- XOR
+            --Result <= A xor B;
+            --NZVC(3) <= Result(7);
+            --if (Result = x"00") then NZVC(2) <= '1'; else NZVC(2) <= '0'; end if;
+            ---NZVC(1) <= '0';
+            --NZVC(0) <= '0';
+
+        --elsif (ALU_Sel = "101") then  -- NOT A
+           -- Result <= not A;
+            --NZVC(3) <= Result(7);
+            --if (Result = x"00") then NZVC(2) <= '1'; else NZVC(2) <= '0'; end if;
+            --NZVC(1) <= '0';
+            --NZVC(0) <= '0';
+
+        --elsif (ALU_Sel = "110") then  -- SHIFT LEFT
+            --Result <= A(6 downto 0) & '0';
+            --NZVC(3) <= Result(7);
+            --if (Result = x"00") then NZVC(2) <= '1'; else NZVC(2) <= '0'; end if;
+            --NZVC(1) <= '0';
+            --NZVC(0) <= A(7);
+
+        --elsif (ALU_Sel = "111") then  -- SHIFT RIGHT
+            --Result <= '0' & A(7 downto 1);
+            --NZVC(3) <= Result(7);
+            --if (Result = x"00") then NZVC(2) <= '1'; else NZVC(2) <= '0'; end if;
+            ---NZVC(1) <= '0';
+            --NZVC(0) <= A(0);
+
+         else
+				Result <= (others => '0');
+				NZVC <= (others => '0');
+	
+	    end if;
+	
+end process;
+
+end arch_alu;
+
+
+
+		
+		
---
+++ alu_test.vhd
@@ -0,0 +1,83 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity alu_test is
+    port(
+        A_switches : in std_logic_vector(7 downto 0);
+        B_switches : in std_logic_vector(7 downto 0);
+        ALU_Sel    : in std_logic_vector(1 downto 0);
+
+        -- Salidas a displays
+        display1_B : out std_logic_vector(6 downto 0);
+        display2_B : out std_logic_vector(6 downto 0);
+		  display1_R : out std_logic_vector(6 downto 0);
+        display2_R : out std_logic_vector(6 downto 0);
+		  NZVC_leds  : out std_logic_vector(3 downto 0)
+    );
+end alu_test;
+
+architecture arch_alu_test of alu_test is
+
+    component alu 
+        port(
+            A        : in  std_logic_vector(7 downto 0);
+            B        : in  std_logic_vector(7 downto 0);
+            ALU_Sel  : in  std_logic_vector(1 downto 0);
+            Result   : out std_logic_vector(7 downto 0);
+            NZVC     : out std_logic_vector(3 downto 0)
+        );
+    end component;
+
+    component DecoHexadecimal 
+        port(
+           A         : in std_logic_vector(3 downto 0);
+           Display_0 : out std_logic_vector(6 downto 0)
+        );
+    end component;
+
+    -- Señales internas de la ALU
+    signal alu_result : std_logic_vector(7 downto 0);
+    signal alu_flags  : std_logic_vector(3 downto 0);
+
+begin
+
+    ALU1 : alu
+        port map(
+            A        => A_switches,
+            B        => B_switches,
+            ALU_Sel  => ALU_Sel,
+            Result   => alu_result,
+            NZVC     => alu_flags
+        );
+   
+	NZVC_leds <= alu_flags;
+
+    -- Displays de B
+    DISPLAY1B : DecoHexadecimal
+        port map (
+            A => B_switches(7 downto 4),
+            Display_0 => display1_B
+        );
+
+    DISPLAY2B : DecoHexadecimal
+        port map (
+            A => B_switches(3 downto 0),
+            Display_0 => display2_B
+        );
+		  
+	 -- Displays de Resultado
+    DISPLAY1R : DecoHexadecimal
+        port map (
+            A => alu_result(7 downto 4),
+            Display_0 => display1_R
+        );
+
+    DISPLAY2R : DecoHexadecimal
+        port map (
+            A => alu_result(3 downto 0),
+            Display_0 => display2_R
+        );
+
+end arch_alu_test;
+
